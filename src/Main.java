17b8bc8f68c7f63aaa4601d44c13c1b8aebdb34f
KergmaHyper <kergma@gmail.com>
2021-06-02 12:05:10 +0300

    first not empty

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Class a= String.class;
+        System.out.println(a);
+        a = "Hello".getClass();
+        System.out.println(a);
+
+    }
+}
